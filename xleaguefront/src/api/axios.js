f6076007cc84006659ba2b16fddc0327ddc31a92
mohamedalirz <mohammedalirzeygui@gmail.com>
2026-05-07 05:56:16 +0100

    Change axios base URL to production endpoint

--- xleaguefront/src/api/axios.js
+++ xleaguefront/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8082/api',
+  baseURL: 'https://xleagueapp.onrender.com/api',
   timeout: 10000,
 });
 
