f833343b1629157b1081378f3ff424b5254eb0e9
mohamedalirz <mohammedalirzeygui@gmail.com>
2026-05-07 06:16:19 +0100

    Change CORS origin to 'http://localhost:3000'
    Updated CORS configuration to restrict origins.

--- XLeague/demo/src/main/java/com/XLeague/demo/Controller/AuthController.java
+++ XLeague/demo/src/main/java/com/XLeague/demo/Controller/AuthController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     @Autowired
