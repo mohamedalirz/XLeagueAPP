d478e55504019feb424e89f982a4728ada753ce9
mohamedalirz <mohammedalirzeygui@gmail.com>
2026-05-07 06:13:25 +0100

    Update SecurityConfig.java

--- XLeague/demo/src/main/java/com/XLeague/demo/Config/SecurityConfig.java
+++ XLeague/demo/src/main/java/com/XLeague/demo/Config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
