6e08b3a5d72d861f12798b721f112cecdcf3ec21
mohamedalirz <mohammedalirzeygui@gmail.com>
2026-05-07 06:04:08 +0100

    Change CORS policy to allow all origins

--- XLeague/demo/src/main/java/com/XLeague/demo/Controller/AuthController.java
+++ XLeague/demo/src/main/java/com/XLeague/demo/Controller/AuthController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
